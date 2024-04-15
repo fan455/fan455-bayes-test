22fb6761f07e4a90f68b25cff13901e6886fa73b
fan455 <66545913+fan455@users.noreply.github.com>
2024-04-16 07:46:04 +0800

    Update plr_bayes.bat

--- examples/plr_bayes.bat
+++ examples/plr_bayes.bat
@@ -1,2 +1,2 @@
-plr_bayes.exe T=10 seed=82300 random_seed=true y_path="data/data_y.npy" X_path="data/data_X.npy" sample_data_path="data/data_beta.npy" params=[0,1] H0=[0.,0.] n_warmup=10000 n_sample=10000
+plr_bayes.exe T=10 seed=82300 random_seed=true y_path="data/data_y.npy" X_path="data/data_X.npy" sample_data_path="data/data_beta.npy" x_index=[0,1] H0=[0,0] n_warmup=10000 n_sample=10000
 pause
